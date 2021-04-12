ec5b47ba1586ca72f7999e3783e7cf74635f5f41
cbc1125 <62823982+cbc1125@users.noreply.github.com>
2021-04-12 22:26:57 +0900

    add SceneManager
    씬 전환 매니져 작업중 (아직 미적용)

---
+++ Assets/Scenes/SceneManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneManager : MonoBehaviour
+{
+    public GameObject player;
+
+    Vector3 startingPos;
+    Quaternion startingRotate;
+    bool isStarted = false;
+
+    static int stageNum = 0;
+
+    private void Awake()
+    {
+        Time.timeScale = 0f;
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
