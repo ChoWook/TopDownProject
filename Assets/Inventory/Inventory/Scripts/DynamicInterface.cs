ffc4510c30fcdc190b13606f9e0cf5ebce203dba
ChoWook <41852462+ChoWook@users.noreply.github.com>
2021-04-28 21:57:34 +0900

    Add Equipment System
    Add Equipment System
    but runtime error exist

---
+++ Assets/Inventory/Inventory/Scripts/DynamicInterface.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DynamicInterface : UserInterface
+{
+    public GameObject inventoryPrefab;
+    public int X_START = -93;
+    public int Y_START = 140;
+    public int X_SPACE_BETWEEN_ITEM = 47;
+    public int NUMBER_OF_COLUMN = 5;
+    public int Y_SPACE_BETWEEN_ITEMS = 47;
+
+    public override void CreateSlots()
+    {
+        itemsDisplayed = new Dictionary<GameObject, InventorySlot>();
+        for (int i = 0; i < inventory.Container.Items.Length; i++)
+        {
+            var obj = Instantiate(inventoryPrefab, Vector3.zero, Quaternion.identity, transform);
+            obj.GetComponent<RectTransform>().localPosition = GetPosition(i);
+
+            AddEvent(obj, EventTriggerType.PointerEnter, delegate { OnEnter(obj); });
+            AddEvent(obj, EventTriggerType.PointerExit, delegate { OnExit(obj); });
+            AddEvent(obj, EventTriggerType.BeginDrag, delegate { OnDragStart(obj); });
+            AddEvent(obj, EventTriggerType.EndDrag, delegate { OnDragEnd(obj); });
+            AddEvent(obj, EventTriggerType.Drag, delegate { OnDrag(obj); });
+
+
+            itemsDisplayed.Add(obj, inventory.Container.Items[i]);
+        }
+    }
+    private Vector3 GetPosition(int i)
+    {
+        return new Vector3(X_START + (X_SPACE_BETWEEN_ITEM * (i % NUMBER_OF_COLUMN)), Y_START + (-Y_SPACE_BETWEEN_ITEMS * (i / NUMBER_OF_COLUMN)), 0f);
+    }
+}
---
+++ Assets/Inventory/Inventory/Scripts/Player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour // 플레이어 인벤토리 클래스
+                                    // 플레이어 캐릭터가 하나씩 가져야함
+{
+    public MouseItem mouseItem = new MouseItem();
+
+    public InventoryObject inventory;
+    // Start is called before the first frame update
+
+    public void OnTriggerEnter(Collider other)
+    {
+        var item = other.GetComponent<GroundItem>();
+        if (item)
+        {
+            Item _item = new Item(item.item);
+            Debug.Log(_item.Id);
+            inventory.AddItem(_item, 1);
+            Destroy(other.gameObject);
+        }
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            inventory.Save();
+        }
+        if (Input.GetKeyDown(KeyCode.KeypadEnter))
+        {
+            inventory.Load();
+        }
+    }
+    private void OnApplicationQuit()
+    {
+        inventory.Container.Items = new InventorySlot[35];
+    }
+}
