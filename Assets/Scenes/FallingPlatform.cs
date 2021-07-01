a8cb4b4c14cd179d519cb60cd82e2128375471bd
ParkHwanYong <bhy7748@naver.com>
2021-07-02 03:16:02 +0900

    add falling platform

---
+++ Assets/Scenes/FallingPlatform.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingPlatform : MonoBehaviour
+{
+
+    private Rigidbody2D rbd;
+
+    public float fallplat;
+
+    private void Start()
+    {
+        rbd = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.collider.CompareTag("Player"))
+        {
+            StartCoroutine(Fall());
+        }
+    }
+
+
+    IEnumerator Fall()
+    {
+        yield return new WaitForSeconds(fallplat);
+        rbd.isKinematic = false;
+        GetComponent<Collider2D>().isTrigger = true;
+        yield return 0;
+    }
+}
