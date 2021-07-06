e36aaf696092bc29c95994fa25918315de802443
ParkHwanYong <bhy7748@naver.com>
2021-07-06 15:31:22 +0900

    respawn falling platform

--- Assets/Scenes/FallingPlatform.cs
+++ Assets/Scenes/FallingPlatform.cs
@@ -5,25 +5,42 @@ using UnityEngine;
 public class FallingPlatform : MonoBehaviour
 {
 
+    [SerializeField] float fallTime = 0.5f, returnTime = 2f;
     Rigidbody2D rb;
-    float DelayFall = 2f;
+    Vector2 startPos;
+    bool isBack;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        startPos = transform.position;
     }
+
+    void Update()
+    {
+        if (isBack)
+            transform.position = Vector2.MoveTowards(transform.position, startPos, 20 * Time.deltaTime);
+
+        if (transform.position.y == startPos.y)
+            isBack = false;
+    }
+
     void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == ("Player"))
-        {
-            Invoke("Fall", DelayFall);
-            Debug.Log("Hit" + collision.gameObject.name);
-        }
+        if (collision.gameObject.tag == ("Player") && !isBack)
+            Invoke("FallPlatform", fallTime);
     }
-    void Fall()
+
+    void FallPlatform()
     {
         rb.isKinematic = false;
-
+        Invoke("BackPlatform", returnTime);
     }
 
+    void BackPlatform()
+    {
+        rb.velocity = Vector2.zero;
+        rb.isKinematic = true;
+        isBack = true;
+    }
 }
