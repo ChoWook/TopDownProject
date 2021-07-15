ee2c3b9262c4dedc5d325e9e5fce54e7ae9357b3
cbc1125 <62823982+cbc1125@users.noreply.github.com>
2021-07-16 01:06:35 +0900

    Pause Ui base script
    Pause Ui base script update

---
+++ Assets/Script/PauseMenu.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseManu : MonoBehaviour
+{
+
+    public static bool isPaused = false;
+    public GameObject pauseManuCanvas; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseManuCanvas.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    public void Pause()
+    {
+        pauseManuCanvas.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("구현 중");
+        Application.Quit();
+    }
+}
