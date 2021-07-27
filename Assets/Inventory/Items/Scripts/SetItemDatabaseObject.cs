72a7d3f1ba7bc93e085a101a7f34fd893ffb30d1
ChoWook <41852462+ChoWook@users.noreply.github.com>
2021-07-27 18:25:37 +0900

    Add Set Item base scripts

---
+++ Assets/Inventory/Items/Scripts/SetItemDatabaseObject.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Set Item Database", menuName = "Inventory System/Items/SetDatabase")]
+public class SetItemDatabaseObject : ScriptableObject, ISerializationCallbackReceiver
+{
+    public SetItem[] SetItems;
+
+    [ContextMenu("Update ID's")]
+    public void UpdateID()
+    {
+        for (int i = 0; i < SetItems.Length; i++)
+        {
+            if (SetItems[i].Id != i)
+                SetItems[i].Id = i;
+        }
+    }
+    public void OnAfterDeserialize()
+    {
+        UpdateID();
+    }
+
+    public void OnBeforeSerialize()
+    {
+    }
+}
+
+public class SetItem
+{
+    public Item[] Items;
+    public int Id;
+}
