666dec6d5139be791a2159f2db626e0a2864534a
ParkHwanYong <bhy7748@naver.com>
2021-08-08 19:22:28 +0900

    fix falling trap

---
+++ Assets/Scenes/Scripts/FallingTrap.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingTrap : MonoBehaviour
+{
+    Rigidbody2D rb;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void OnTriggerEnter2D (Collider2D col)
+    {
+        if (col.gameObject.tag.Equals("Player"))
+            rb.isKinematic = false;
+    }
+    
+    void OnCollisionEnter2D (Collision2D col)
+    {
+        if(col.gameObject.tag.Equals("player"))
+            Debug.Log("Destroyed");
+            Destroy(gameObject);
+    }
+}
