6bf1f6a6bad9d05681b24d6c10a1ecd3b0c47236
ChoWook <41852462+ChoWook@users.noreply.github.com>
2021-08-26 19:05:08 +0900

    Fix Item tooltip base
    Fix Item tooltip base

--- Assets/Inventory/Items/Scripts/Tooltip.cs
+++ Assets/Inventory/Items/Scripts/Tooltip.cs
@@ -7,30 +7,34 @@ public class Tooltip : MonoBehaviour
 {
     private static Tooltip instance;
 
-    [SerializeField]
-    private Camera uiCamera;
+    public Camera uiCamera;
 
+    public Image ItemImage;
+    public Text NameText;
+    public Text SetNameText;
+    public Text DescriptionText;
+    public Text[] SetItemCheckText;
 
-    private Text TooltipText;
     private RectTransform backgroundRectTransform;
     
     public float textPaddingSize = 4f;
 
     private void Awake()
     {
-        TooltipText = transform.Find("Text").GetComponent<Text>();
-        backgroundRectTransform = transform.Find("Background").GetComponent<RectTransform>();
+
+        //TooltipText = transform.Find("Text").GetComponent<Text>();
+        //backgroundRectTransform = transform.Find("Background").GetComponent<RectTransform>();
 
         ShowTooltip("툴팁이다");
     }
 
     private void Update()
     {
-        Vector2 localPoint;
-        RectTransformUtility.ScreenPointToLocalPointInRectangle(transform.parent.GetComponent<RectTransform>(), Input.mousePosition, uiCamera, out localPoint);
-        transform.localPosition = localPoint;
+        //Vector2 localPoint;
+        //RectTransformUtility.ScreenPointToLocalPointInRectangle(transform.parent.GetComponent<RectTransform>(), Input.mousePosition, uiCamera, out localPoint);
+        //transform.localPosition = localPoint;
 
-        Vector2 anchoredPosition = transform.GetComponent<RectTransform>().anchoredPosition;
+        //Vector2 anchoredPosition = transform.GetComponent<RectTransform>().anchoredPosition;
         //if(anchoredPosition.x + backgroundRectTransform.rect.width > canvasRecttange)
     }
 
@@ -38,9 +42,9 @@ public class Tooltip : MonoBehaviour
     {
         gameObject.SetActive(true);
 
-        TooltipText.text = tooltipString;
-        Vector2 backgroundSize = new Vector2(TooltipText.preferredWidth + textPaddingSize * 2f, TooltipText.preferredHeight + textPaddingSize * 2f);
-        backgroundRectTransform.sizeDelta = backgroundSize;
+        //TooltipText.text = tooltipString;
+        //Vector2 backgroundSize = new Vector2(TooltipText.preferredWidth + textPaddingSize * 2f, TooltipText.preferredHeight + textPaddingSize * 2f);
+        //backgroundRectTransform.sizeDelta = backgroundSize;
     }
 
     private void HideTooltip()
