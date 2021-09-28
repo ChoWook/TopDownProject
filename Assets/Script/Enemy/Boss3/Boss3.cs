3406ae67caffd52f19180c7209e49b4194954e52
ChoWook <41852462+ChoWook@users.noreply.github.com>
2021-09-29 00:00:11 +0900

    Add FallingGrid script
    Add FallingGrid script

--- Assets/Script/Enemy/Boss3/Boss3.cs
+++ Assets/Script/Enemy/Boss3/Boss3.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class Boss3 : MonoBehaviour
 {
-    public GameObject FallingGrid;
     // Start is called before the first frame update
     void Start()
     {
---
+++ Assets/Script/Enemy/Boss3/FallingGrid.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingGrid : MonoBehaviour
+{
+    public float FallingSpeed = 2.5f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(0, transform.position.y - FallingSpeed * Time.deltaTime, 0);
+  
+        if (transform.position.y < -36.0f)
+        {
+            transform.position = new Vector3(0, 36, 0);
+        }
+    }
+}
