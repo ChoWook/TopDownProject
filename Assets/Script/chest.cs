72c25dcca3efbbe2d477d9d73bad1b2d05898309
ParkHwanYong <bhy7748@naver.com>
2021-10-14 04:14:54 +0900

    add chest

---
+++ Assets/Script/chest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class chest : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+    public void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player") && Input.GetKey(KeyCode.UpArrow))
+        {
+            {
+                if (other.gameObject.tag.Equals("player"))
+                    Debug.Log("Destroyed");
+                Destroy(gameObject);
+            }
+        }
+    }
+
+}
