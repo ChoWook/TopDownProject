9f47dab62d021115e3292c9b2b6d3d5defcdfce4
ChoWook <41852462+ChoWook@users.noreply.github.com>
2021-10-27 23:21:10 +0900

    Unity Editor Bug fix
    Unity Editor Bug fix

--- Assets/Inventory/Items/Scripts/GroundItem.cs
+++ Assets/Inventory/Items/Scripts/GroundItem.cs
@@ -1,9 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-#if UNITY_EDITOR
-using UnityEditor;
-#endif
 
 public class GroundItem : MonoBehaviour, ISerializationCallbackReceiver
 {
@@ -12,7 +9,7 @@ public class GroundItem : MonoBehaviour, ISerializationCallbackReceiver
     private void Awake()
     {
         GetComponentInChildren<SpriteRenderer>().sprite = item.uiDisplay;
-        EditorUtility.SetDirty(GetComponentInChildren<SpriteRenderer>());
+        //EditorUtility.SetDirty(GetComponentInChildren<SpriteRenderer>());
     }
     public void OnAfterDeserialize()
     {
@@ -20,9 +17,6 @@ public class GroundItem : MonoBehaviour, ISerializationCallbackReceiver
 
     public void OnBeforeSerialize()
     {
-#if UNITY_EDITOR
-        //GetComponentInChildren<SpriteRenderer>().sprite = item.uiDisplay;
-        //EditorUtility.SetDirty(GetComponentInChildren<SpriteRenderer>());
-#endif
+
     }
 }
